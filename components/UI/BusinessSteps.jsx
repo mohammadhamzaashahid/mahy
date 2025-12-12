62b2c16b860f2ad3294dfc4dc6de01daf786c51a
muhammadhamzasalahuddin <mohammad.hamzaa.shahid@gmail.com>
2025-12-12 20:55:10 +0500

    color change

--- components/UI/BusinessSteps.jsx
+++ components/UI/BusinessSteps.jsx
@@ -43,7 +43,7 @@ function BusinessSteps() {
             </div>
             <div className="flex flex-wrap justify-center items-center mt-50 lg:mt-40 mx-5 gap-2">
                 <p>Stay ahead in a rapidly changing world.</p>
-                <Link className="bg-cyan-500 hover:bg-cyan-600 text-white px-3 py-1 rounded-2xl" href={"/"}>Consult Now</Link>
+                <Link className="bg-teal-600 hover:bg-cyan-600 text-white px-3 py-1 rounded-2xl" href={"/"}>Consult Now</Link>
             </div>
         </section>
     )
