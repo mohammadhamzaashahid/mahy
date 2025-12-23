180b3674193e4761f16ca033f78e2dd82f18f2ef
Muhammad Bin Arshad <mbinac1199@gmail.com>
2025-12-23 11:57:32 +0500

    history design changes

--- components/UI/about-us/HistoryCard.jsx
+++ components/UI/about-us/HistoryCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function HistoryCard({ title, subText, image, description, index }) {
     return (
-        <div className={`w-[90vw] lg:w-[35vw] flex-none relative z-20 ${index % 2 !== 0 && "top-70"}`}>
-            <div className='border-l pt-2 pb-8 relative pl-4'>
+        <div className={`w-[90vw] lg:w-[35vw] flex-none relative z-20`}>
+            <div className={`border-l pt-2 pb-8 relative pl-4 ${index % 2 !== 0 && "top-70"}`}>
                 <p className='font-bold text-sm uppercase tracking-[0.28em] text-[#18a2e2]'>{title}</p>
                 <p className='text-sm font-semibold mt-1'>{subText}</p>
                 <ul className="list-disc list-inside space-y-1 mt-3">
@@ -20,7 +20,10 @@ function HistoryCard({ title, subText, image, description, index }) {
                         </li>
                     ))}
                 </ul>
-                {/* <div className="relative h-30 w-full mt-3">
+                <div className="absolute bottom-0 -left-2 h-4 w-4 rounded-full b-base"></div>
+            </div>
+            <div className="absolute inset-0 left-5 right-15">
+                <div className={`relative h-50 w-full flex-none ${index % 2 === 0 && "top-70"}`}>
                     <Image
                         src={image}
                         alt={title}
@@ -28,8 +31,7 @@ function HistoryCard({ title, subText, image, description, index }) {
                         style={{ objectFit: "cover" }}
                         priority
                     />
-                </div> */}
-                <div className="absolute bottom-0 -left-2 h-4 w-4 rounded-full b-base"></div>
+                </div>
             </div>
         </div>
     )
